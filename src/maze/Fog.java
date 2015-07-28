e6e0d230e2e6a8e3edbb5f37233b6e59eb18c333
brianlocksa <brianlocksa@gmail.com>
2015-07-28 15:03:18 -0700

    Update to Fog class changed to have fewer variables.

--- src/maze/Fog.java
+++ src/maze/Fog.java
@@ -7,21 +7,19 @@ import javax.swing.ImageIcon;
 public class Fog {
 	
 	private Image fog, fogOpaque;
-	private int fogMapA = 14;
-	private int fogMapB = 14;
-	private int[][] fogMap = new int[fogMapA][fogMapB];
+	private int fogMapSize = 14;
+	private int[][] fogMap = new int[fogMapSize][fogMapSize];
 	
 	public Fog(){
-		ImageIcon img = new ImageIcon("src/resources/water.png");
-		img = new ImageIcon("src/resources/fog.png");
+		ImageIcon img = new ImageIcon("src/resources/fog.png");
 		fog = img.getImage();
 		img = new ImageIcon("src/resources/fog-opaque.png");
 		fogOpaque = img.getImage();
 	}
 		
 	public void reFog(int x, int y) {
-		for(int i = 0; i < fogMapA; i++) {
-			for (int j = 0; j < fogMapB; j++){
+		for(int i = 0; i < fogMapSize; i++) {
+			for (int j = 0; j < fogMapSize; j++){
 				if(i == x && j == y) {
 					fogMap[i][j] = 0;
 				} else if(i == x && j == (y - 1)){
@@ -39,8 +37,8 @@ public class Fog {
 	}
 	
 	public void createFog(int x, int y) {
-		for(int i = 0; i < fogMapA; i++) {
-			for (int j = 0; j < fogMapB; j++){
+		for(int i = 0; i < fogMapSize; i++) {
+			for (int j = 0; j < fogMapSize; j++){
 				if(i == x && j == y) {
 					fogMap[i][j] = 0;
 				} else if(i == x && j == (y - 1)){
